0530f02e35e7a1c440a32854c66ec9d3eb743305
gaurav.singh-at-078399860782 <8KUFfGQ8p6dqZA2Pgw3YcvlMaYN2LWvDR8pNHeyy+Ik=>
2020-02-28 10:07:42 +0100

    Correct endpoint

--- src/main/java/com/ing/testcase/farmshop/farmshop/common/Constants.java
+++ src/main/java/com/ing/testcase/farmshop/farmshop/common/Constants.java
@@ -3,7 +3,7 @@ package com.ing.testcase.farmshop.farmshop.common;
 public interface Constants {
 
     String CONTROLLER_END_POINT = "/farmshop";
-    String FLOCK_END_POINT = "/flocks";
+    String FLOCK_END_POINT = "/flock";
     String STOCK_END_POINT = "/stock";
     String ORDER_END_POINT = "/order";
 }
